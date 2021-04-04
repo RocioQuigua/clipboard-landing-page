ff09e0d7819541b09d75b9a23f5aab669d0d99fd
RocioQuigua <rocioquigualopez@gmail.com>
2021-04-04 15:52:10 -0500

    feat(SectionKeep): add section

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import SectionAccess from './components/SectionAccess/SectionAccess';
+import SectionKeep from './components/SectionKeep/SectionKeep';
 
 export default function App() {
   return (
     <div>
+      <SectionKeep/>
       <SectionAccess/>
       <Footer/>
     </div>
---
+++ src/components/SectionKeep/SectionKeep.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import './SectionKeep.css';
+import imgKeep from '../../assets/img/image-computer.png';
+
+export default function SectionKeep() {
+  return (
+    <div className='section-keep'>
+      <div className='container-tittle'>
+        <h1>Keep track of your snippets</h1>
+        <p>Clipboard instatly stores any you copy in the clous, meaning you can access
+        your snippets immediately on all your devices. Our Mac and iOS apps will
+          help you organize everything.</p>
+      </div>
+      <div className='content'>
+        <img className='img-keep' src={imgKeep} alt="imgKeep" />
+        <div className='container-item'>
+          <div className='card-item'>
+            <h2>Quick Search</h2>
+            <p>Easily search your snippets by content,
+          category, web address, application, and more.</p>
+          </div>
+          <div className='card-item'>
+            <h2>iCloud Sync</h2>
+            <p>Instanly saves and syncs strippets across all
+            your devices.</p>
+          </div>
+          <div className='card-item'>
+            <h2>Complete History</h2>
+            <p>Retrieve any snippets from the first moment
+            you started using the app.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
