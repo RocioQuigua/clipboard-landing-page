e472535f2b7e31a1f6aba2f62acb909a74c52800
RocioQuigua <rocioquigualopez@gmail.com>
2021-04-03 23:10:23 -0500

    feat(SectionAccess): add section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './App.css';
 import Footer from './components/Footer/Footer';
+import SectionAccess from './components/SectionAccess/SectionAccess';
 
 export default function App() {
   return (
     <div>
+      <SectionAccess/>
       <Footer/>
     </div>
   )
---
+++ src/components/SectionAccess/SectionAccess.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './SectionAccess.css';
+import imgAccess from '../../assets/img/image-devices.png'
+
+export default function SectionAccess() {
+  return (
+    <div className='section-access'>
+      <div className='container-tittle'>
+        <h1>Access Clipboard Anywhere</h1>
+        <p>Wheter you're on the go, or at your computer, you can access all
+        your Clipboard snippets in a few simple clicks.</p>
+      </div>
+      <img className='img-access' src={imgAccess} alt="imgAccess" />
+    </div>
+  )
+}
