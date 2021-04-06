65b6536a44f8ef941aa6f7563dcd17ec7f01328f
RocioQuigua <rocioquigualopez@gmail.com>
2021-04-05 19:55:01 -0500

    feat(SectionPresentation): add section

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import SectionAccess from './components/SectionAccess/SectionAccess';
 import SectionKeep from './components/SectionKeep/SectionKeep';
+import SectionPresentation from './components/SectionPresentation/SectionPresentation';
 
 export default function App() {
   return (
     <div>
+      <SectionPresentation/>
       <SectionKeep/>
       <SectionAccess/>
       <Footer/>
---
+++ src/components/SectionPresentation/SectionPresentation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './SectionPresentation.css';
+import logo from '../../assets/img/logo.svg';
+
+export default function SectionPresentation() {
+  return (
+    <div className='section-presentation'>
+      <div className='container-logo'>
+        <img className='logo' src={logo} alt='logo' />
+      </div>
+      <div className='container-tittle'>
+        <h1>A history of everything you copy</h1>
+        <p>Clipboard allows you to track and organize everything you
+          copy. Instanly access your clipboard all your devices.</p>
+      </div>
+      <div className='container-button'>
+        <button className='button-primary'>Download for iOS</button>
+        <button className='button-second'>Download for Mac</button>
+      </div>
+    </div>
+  )
+}
