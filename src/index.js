94ff1be73ff34272bf000fcec84a30f4a5c7392c
RocioQuigua <rocioquigualopez@gmail.com>
2021-04-19 14:15:10 -0500

    feat: architecture scss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './sass/main.scss';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
